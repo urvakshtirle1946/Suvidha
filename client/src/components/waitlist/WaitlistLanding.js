0a173b08d6458f168094dc42171f13d26d553e5e
urvakshtirle1946 <urvakshtirle@gmail.com>
2026-03-04 00:06:40 +0530

    fixed bugs

--- client/src/components/waitlist/WaitlistLanding.js
+++ client/src/components/waitlist/WaitlistLanding.js
@@ -105,18 +105,19 @@ export default function WaitlistLanding() {
         height: '60%',
         zIndex: 0,
         pointerEvents: 'none',
-        overflow: 'hidden'
+        overflow: 'hidden',
+        filter: 'blur(8px)'
       }}>
         {/* We use multiple SVG waves with opacity to create a smooth layered transition */}
-        <svg viewBox="0 0 1440 320" style={{ position: 'absolute', bottom: 0, width: '200%', height: '100%', left: '0', animation: 'wave 20s linear infinite', opacity: 0.2 }}>
+        <svg viewBox="0 0 1440 320" style={{ position: 'absolute', bottom: 0, width: '200%', height: '100%', left: '0', animation: 'wave 20s linear infinite', opacity: 0.08 }}>
           <path fill="#0026e9" fillOpacity="1" d="M0,192L48,197.3C96,203,192,213,288,229.3C384,245,480,267,576,250.7C672,235,768,181,864,181.3C960,181,1056,235,1152,234.7C1248,235,1344,181,1392,154.7L1440,128L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
           <path transform="translate(1440, 0)" fill="#0026e9" fillOpacity="1" d="M0,192L48,197.3C96,203,192,213,288,229.3C384,245,480,267,576,250.7C672,235,768,181,864,181.3C960,181,1056,235,1152,234.7C1248,235,1344,181,1392,154.7L1440,128L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
         </svg>
-        <svg viewBox="0 0 1440 320" style={{ position: 'absolute', bottom: 0, width: '200%', height: '80%', left: '0', animation: 'wave 15s linear infinite reverse', opacity: 0.3 }}>
+        <svg viewBox="0 0 1440 320" style={{ position: 'absolute', bottom: 0, width: '200%', height: '80%', left: '0', animation: 'wave 15s linear infinite reverse', opacity: 0.15 }}>
           <path fill="#0026e9" fillOpacity="1" d="M0,224L48,213.3C96,203,192,181,288,186.7C384,192,480,224,576,240C672,256,768,256,864,229.3C960,203,1056,149,1152,144C1248,139,1344,181,1392,202.7L1440,224L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
           <path transform="translate(1440, 0)" fill="#0026e9" fillOpacity="1" d="M0,224L48,213.3C96,203,192,181,288,186.7C384,192,480,224,576,240C672,256,768,256,864,229.3C960,203,1056,149,1152,144C1248,139,1344,181,1392,202.7L1440,224L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
         </svg>
-        <svg viewBox="0 0 1440 320" style={{ position: 'absolute', bottom: 0, width: '200%', height: '60%', left: '0', animation: 'wave 10s linear infinite', opacity: 0.6 }}>
+        <svg viewBox="0 0 1440 320" style={{ position: 'absolute', bottom: 0, width: '200%', height: '60%', left: '0', animation: 'wave 10s linear infinite', opacity: 0.3 }}>
           <path fill="#0026e9" fillOpacity="1" d="M0,128L48,144C96,160,192,192,288,181.3C384,171,480,117,576,106.7C672,96,768,128,864,160C960,192,1056,224,1152,213.3C1248,203,1344,149,1392,122.7L1440,96L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
           <path transform="translate(1440, 0)" fill="#0026e9" fillOpacity="1" d="M0,128L48,144C96,160,192,192,288,181.3C384,171,480,117,576,106.7C672,96,768,128,864,160C960,192,1056,224,1152,213.3C1248,203,1344,149,1392,122.7L1440,96L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path>
         </svg>

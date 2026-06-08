1986f1c7b056ecae01a93d1148bf2f1b4f62890a
Urvaksh Tirle <urvaksh.tirle@cdgi.edu.in>
2026-06-08 13:57:37 +0530

    fix: prefer gmail starttls on render

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -24,6 +24,7 @@ const adminLoginLimiter = rateLimit({
 
 router.post('/register', authLimiter, authController.register);
 router.post('/login', authLimiter, authController.login);
+router.get('/mail-status', authController.mailStatus);
 router.get('/test-email', authController.testEmail);
 router.post('/google', authLimiter, authController.googleLogin);
 router.post('/complete-google', authLimiter, authController.completeGoogleRegistration);
